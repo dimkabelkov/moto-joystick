2df7679e324e185444657949be6365c222bb9d44
Dmitriy Kiryukhin <dimkabelkov@gmail.com>
2018-06-05 01:21:42 +0300

    support relay

--- jostic/jostic.ino
+++ jostic/jostic.ino
@@ -1,3 +1,4 @@
+
 #include  <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -5,14 +6,19 @@ RF24 radio(7,8);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 const int switchPin = 9;
-const int pinX = A1;
-const int pinY = A2;
+const int pinX = A2;
+const int pinY = A1;
 
 bool isRun = false;
 bool lasuUp = true;
 
 int lastX = 0;
 int lastY = 0;
+
+int dX = 0;
+int dY = 0;
+
+int deltaTime = 1000;
  
 void setup() {
 
@@ -25,38 +31,91 @@ void setup() {
 
   radio.begin();
   radio.setChannel(2);
-  radio.setPayloadSize(12);
+  radio.setPayloadSize(5);
   radio.setDataRate(RF24_250KBPS);
   radio.openWritingPipe(pipe);
+
+  Serial.println("Setup ...");
 }
  
 void loop() {
+  int X = analogRead(pinX) - 512;
+  int Y = analogRead(pinY) - 512;
+  
+  if (deltaTime) {
+    dX = X;
+    dY = Y;
+
+    deltaTime --;
+    if (!deltaTime) {
+        Serial.println("dX: " + String(dX));
+        Serial.println("dY: " + String(dY));
+    }
+  }
+
+  byte bytes[5] = {0, 0, 0, 0, 0};
+  
   bool switchState = !digitalRead(switchPin);
   if (switchState && !isRun && lasuUp) {
     isRun = true;
     lasuUp = false;
+    bytes[0] = 1;
+    radio.write(bytes, 5);
+    Serial.println("Start ...");
   } else if (switchState && isRun && lasuUp) {
     isRun = false;
     lasuUp = false;
+    radio.write(bytes, 5);
+    Serial.println("Stop ...");
   } else if (isRun) {
-    int X = analogRead(pinX) - 512; // считываем аналоговое значение оси Х
-    int Y = analogRead(pinY) - 512; // считываем аналоговое значение оси Y
+    X -= dX;
+    Y -= dY;
+    bytes[0] = 2;
+    if (abs(X - lastX) > 2 || abs(Y - lastY) > 2) {
+      Serial.println("     X: " + String(X) + ",      Y: " + String(Y));
+      int bX = abs(X);
+      int bIndex = 2;
+      while(bX) {
+        bytes[bIndex] = bX & 255;
+        bX = bX >> 8;
+        bIndex--;
+      }
+      if (X > 0) {
+        bytes[1] = bytes[1] | B00000100;      
+      }
 
-    int diffX = abs(X - lastX);
-    int diffY = abs(Y - lastY);
-    
-    if (diffX > 2 || diffY > 2) {
+      int bY = abs(Y);
+      bIndex = 4;
+      while(bY) {
+        bytes[bIndex] = bY & 255;
+        bY = bY >> 8;
+        bIndex--;
+      }
+      if (Y > 0) {
+        bytes[3] = bytes[3] | B00000100;      
+      }
+      radio.write(bytes, 5);
+
+      int checkX = 0;
+      int checkY = 0;
+      
+      checkX = bytes[1] &~ B00000100;
+      checkX = checkX << 8 | bytes[2];
+      if (!(bytes[1] & B00000100)) {
+        checkX = checkX * -1;
+      }
+
+      checkY = bytes[3] &~ B00000100;
+      checkY = checkY << 8 | bytes[4];
+
+      if (!(bytes[3] & B00000100)) {
+        checkY = checkY * -1;
+      }
+
+      Serial.println("checkX: " + String(checkX) + ", checkY: " + String(checkY));
+      
       lastX = X;
       lastY = Y;
-
-      char copy[11];
-      
-      String send = "";
-      send = ">" + String(X) + ":" + String(Y) + "<";
-
-      send.toCharArray(copy, send.length());
-      Serial.println(send + "," + String(send.length()));
-      radio.write(copy, send.length());
     }
   }
   if (!switchState) {
--- rotate/rotate.ino
+++ rotate/rotate.ino
@@ -11,11 +11,28 @@ const bool motorRotatePosition[8][4] = {
   { 1, 0, 0, 1}
 };
 byte motorRotatePositionSize = 8;
-
 byte motorRotatePositionIndex = 0;
 
 int rotate = 0;
 int rotateNew = 0;
+
+int motor1Speed1 = 5; 
+int motor1Speed2 = 18;
+int motor1SpeedPwm = 23;
+
+int speed = 0;
+int speedNew = 0;
+
+byte relayPin = 19;
+
+bool isRun = false;
+
+bool isDebug = false;
+
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int dutyCycle = 200;
 
 int iterateSerial = 0;
 bool readSerial = false;
@@ -28,7 +45,6 @@ byte command[128];
 RF24 radio(12, 14, 26, 25, 27);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
-
 void setup()
 {
   Serial.begin(115200);
@@ -37,61 +53,133 @@ void setup()
     digitalWrite(motorRotatePin[i], LOW);
   }
 
- radio.begin();
- radio.setChannel(2);
- radio.setPayloadSize(12);
- radio.setDataRate(RF24_250KBPS);
- radio.openReadingPipe(1,pipe);
- radio.startListening();
+  pinMode(motor1Speed1, OUTPUT);
+  pinMode(motor1Speed2, OUTPUT);
+  pinMode(motor1SpeedPwm, OUTPUT);
+
+  pinMode(relayPin, OUTPUT);
+
+  ledcSetup(pwmChannel, freq, resolution);
+  
+  ledcAttachPin(motor1SpeedPwm, pwmChannel);
+
+  radio.begin();
+  radio.setChannel(2);
+  radio.setPayloadSize(5);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
 }
 
 void loop()
 {
-  if (Serial.available() && !radio.available()) {
-    if (!commandLength) {
-      readSerial = true;
-    }
-    command[commandLength++] = Serial.read();
-    iterateSerial = 300;
-  } else if (radio.available() && !Serial.available()) {
+//  if (Serial.available() && !radio.available()) {
+//    if (!commandLength) {
+//      readSerial = true;
+//    }
+//    command[commandLength++] = Serial.read();
+//    iterateSerial = 300;
+//  } else if (radio.available() && !Serial.available()) {
+  if (radio.available()) {
       String xData = "";
       bool isX = false;
-      byte joystickData[12];
-      radio.read(joystickData, 12);      
-      for(int i = 1; i < 12; i++) {
-        if (joystickData[i] != 0) {
-          if (isX) {
-            xData += char(joystickData[i]);
-          } else if (String(char(joystickData[i])) == ":") {
-            isX = true;
-          }
+      byte joystickData[5];
+      radio.read(joystickData, 5);
+
+      if (isRun) {
+        int checkX = 0;
+        int checkY = 0;
+        
+        checkX = joystickData[1] &~ B00000100;
+        checkX = checkX << 8 | joystickData[2];
+        if (!(joystickData[1] & B00000100)) {
+          checkX = checkX * -1;
         }
+  
+        checkY = joystickData[3] &~ B00000100;
+        checkY = checkY << 8 | joystickData[4];
+  
+        if (!(joystickData[3] & B00000100)) {
+          checkY = checkY * -1;
+        }
+        
+        if (isDebug) {
+          Serial.println("checkX: " + String(checkX) + ", checkY: " + String(checkY));
+        }
+
+        rotateNew = checkX;
+        speedNew = checkY;
+          
+      } else if (rotateNew != 0 || speedNew != 0) {
+        rotateNew = 0;
+        speedNew = 0;
       }
-      Serial.println("NRF: " + xData);
-      rotateNew = xData.toInt() * -1;
+
+      if (rotate != rotateNew && isDebug) {
+        Serial.println("rotateNew: " + String(rotateNew));
+      }
+
+      if (speed != speedNew && isDebug) {
+        Serial.println("speedNew: " + String(speedNew));
+      }
+
+      if (joystickData[0] == 1) {
+        isRun = true;
+        if (isDebug) {
+          Serial.println("Start...");
+        }
+        digitalWrite (relayPin, HIGH); 
+      } else if (joystickData[0] == 0) {
+        isRun = false;
+        if (isDebug) {
+          Serial.println("Stop..."); 
+        }
+        digitalWrite (relayPin, LOW);
+      }
+      
   } else if (readSerial && !(iterateSerial--)) {
-    readSerial = false;
-    String xData = "";
-    bool isX = false;
-    for(byte i = 0; i < commandLength; i++)  {
-      if (isX) {
-        xData += char(command[i]);
-      } else if (String(char(command[i])) == ":") {
-        isX = true;
-      }
-    }
-    Serial.println("Serial: " + xData);
-    rotateNew = xData.toInt() * -1;
-    commandLength = 0;
+//    readSerial = false;
+//    String xData = "";
+//    bool isX = false;
+//    for(byte i = 0; i < commandLength; i++)  {
+//      if (isX) {
+//        xData += char(command[i]);
+//      } else if (String(char(command[i])) == ":") {
+//        isX = true;
+//      }
+//    }
+//    Serial.println("Serial: " + xData);
+//    rotateNew = xData.toInt() * -1;
+//    commandLength = 0;
   }
 
   rotateHandler();
+  speedHandler();
+}
+
+void speedHandler()
+{
+  if (abs(speedNew) < 15) {
+    digitalWrite (motor1Speed1, LOW);
+    digitalWrite (motor1Speed2, LOW); 
+    ledcWrite(pwmChannel, 0);
+  } else if (speed != speedNew) {
+    speed = speedNew;
+    int speedMath = abs(speed) - 15;
+    if (speedMath > 490) {
+      speedMath = 490;
+    }
+    if (isDebug) {
+      Serial.println("speed PWM: " + String(150 + speedMath / 4.7));
+    }
+    ledcWrite(pwmChannel, 150 + speedMath / 4.7);
+    digitalWrite (motor1Speed1, speed > 0 ? HIGH : LOW);
+    digitalWrite (motor1Speed2, speed > 0 ? LOW : HIGH);
+  }
 }
 
 void rotateHandler()
 {
-  //Serial.println("rotate: " + String(rotate));
-  //Serial.println("rotateNew: " + String(rotateNew));
   if (rotate != rotateNew) {
     if (rotateNew - rotate > 0) {
       rotate ++;
